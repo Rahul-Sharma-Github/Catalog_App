6d35098f36c71b6f6d1396fe9cb039a241573a47
Rahul <rahulsharma3258@gmail.com>
2022-10-04 16:49:48 +0530

    Second Commit__Added External Dart File for Home Page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,7 +9,8 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     const int days=30;
-    const String name='Codepur';
+    const String name='Codepur.';
+
 
     return Material(
         child: Center(child: Container(child:  const Text('Welcome to $days days of flutter by $name'),),),
